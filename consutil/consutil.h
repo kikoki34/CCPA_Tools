3a13314b855562ffd649f5e1e4bdf61d7cbdc238
kikoki34 <kikoki@tutamail.com>
2024-07-03 18:10:45 +0300

    update server config

---
+++ consutil/consutil.h
@@ -0,0 +1,16 @@
+#ifndef _INC_STDLIB
+#include <stdlib.h>
+#endif
+#ifndef _INC_STDIO
+#include <stdio.h>
+#endif
+
+#ifndef bool
+#define bool char
+#endif
+#ifndef false
+#define false 0
+#endif
+#ifndef true
+#define true 1
+#endif
